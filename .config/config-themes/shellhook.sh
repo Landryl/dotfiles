f5f90b3e4c57d84fb06bcf5f9185d4b886121b6f
Landryl <Landryl@users.noreply.github.com>
2019-02-26 22:15:15 +0100

    Checking if theme exists before applying

--- .config/config-themes/shellhook.sh
+++ .config/config-themes/shellhook.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-. ~/.config/config-themes/config
-bash ~/.config/config-themes/$CONFIGTHEME/shellcolors.sh
+source $HOME/.config/config-themes/config
+source $HOME/.config/config-themes/$CONFIGTHEME/shellcolors.sh
 
 function theme() {
-  echo "CONFIGTHEME=$1" > ~/.config/config-themes/config
-  bash ~/.config/config-themes/$CONFIGTHEME/shellcolors.sh
-  i3-msg restart
+  if [ -d "$HOME/.config/config-themes/$1/" ]; then
+    echo "CONFIGTHEME=$1" > ~/.config/config-themes/config
+    source $HOME/.config/config-themes/shellhook.sh
+    i3-msg restart
+    return 0
+  else
+    echo "Error: unknown theme $1"
+    return 1
+  fi
 }
