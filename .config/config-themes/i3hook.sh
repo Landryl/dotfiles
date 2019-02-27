1474b07f2cb28c3ddbc84672dcf0b270e2b5019f
Landryl <Landryl@users.noreply.github.com>
2019-02-27 21:29:54 +0100

    Hooking Xresources to config-themes

--- .config/config-themes/i3hook.sh
+++ .config/config-themes/i3hook.sh
@@ -1,4 +1,5 @@
-. ~/.config/config-themes/config
-feh --bg-fill ~/.config/config-themes/$CONFIGTHEME/wallpaper.png
-cp ~/.config/config-themes/$CONFIGTHEME/rofitheme.rasi ~/.config/config-themes/
-cp ~/.config/config-themes/$CONFIGTHEME/conky.conf ~/.config/config-themes/
+source ~/.config/config-themes/config
+feh --bg-fill $HOME/.config/config-themes/$CONFIGTHEME/wallpaper.png
+cp $HOME/.config/config-themes/$CONFIGTHEME/rofitheme.rasi ~/.config/config-themes/
+cp $HOME/.config/config-themes/$CONFIGTHEME/conky.conf ~/.config/config-themes/
+xrdb $HOME/.config/config-themes/$CONFIGTHEME/Xresources
